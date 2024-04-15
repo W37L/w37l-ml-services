17c9ce6d576b8946db343944e23d2d84b0f31a9f
Emi-27 <80519649+Emi-27@users.noreply.github.com>
2024-04-16 00:59:17 +0200

    stopword deleted env

--- src/mlservice/service/service_impl.py
+++ src/mlservice/service/service_impl.py
@@ -1,8 +1,6 @@
 import joblib
 import re
 from bs4 import BeautifulSoup
-
-from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from dataclasses import dataclass
 
@@ -39,9 +37,21 @@ def preprocess_text(text):
     text = re.sub(r'http\S+', '', text) 
     text = re.sub(r'[^a-zA-Z\s]', '', text)
 
-    stop_words = stopwords.words('english')
     review_without_stop_words = ' '.join([word for word in text.split() if word not in stop_words]) 
     stemmer = SnowballStemmer("english") 
     review_stemmed = ' '.join([stemmer.stem(word) for word in
     review_without_stop_words.split()])
     return review_stemmed
+
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
+                   'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they',
+                     'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those',
+                       'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+                         'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
+                           'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
+                             'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
+                               'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
+                                 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own',
+                                   'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd',
+                                     'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven',
+                                       'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
